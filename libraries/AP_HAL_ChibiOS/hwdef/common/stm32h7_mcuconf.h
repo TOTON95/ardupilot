89deddeb7d4795343068ce0bc4a5e6351896e958
Leonardo Garcia <lgarciaos@protonmail.com>
2023-02-18 20:40:30 -0800

    Add support for M10059E

--- libraries/AP_HAL_ChibiOS/hwdef/common/stm32h7_mcuconf.h
+++ libraries/AP_HAL_ChibiOS/hwdef/common/stm32h7_mcuconf.h
@@ -79,7 +79,7 @@
 #ifdef SMPS_PWR
 #define STM32_PWR_CR3                       (PWR_CR3_SMPSEN | PWR_CR3_USB33DEN)
 #else
-#define STM32_PWR_CR3                       (PWR_CR3_LDOEN | PWR_CR3_USB33DEN)
+#define STM32_PWR_CR3                       (PWR_CR3_BYPASS | PWR_CR3_USB33DEN | PWR_CR3_SCUEN)
 #endif
 #define STM32_PWR_CPUCR                     0
 
